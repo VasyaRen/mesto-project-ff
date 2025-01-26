c0271c00bab5a3f4625cfcf603b04ce69239a36e
Yozhik <vasya.renin@yandex.ru>
2025-01-26 03:48:39 +0300

    create functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const cardsList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(item, removeCard) {
+  const cardItem = cardTemplate.querySelector(".card").cloneNode(true);
+  cardItem.querySelector(".card__image").src = item.link;
+  cardItem.querySelector(".card__title").textContent = item.name;
+
+  const buttonDelete = cardItem.querySelector(".card__delete-button");
+  buttonDelete.addEventListener("click", removeCard);
+
+  return cardItem;
+}
 
 // @todo: Функция удаления карточки
+function removeCard(evt) {
+  evt.target.closest(".card").remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (item) {
+  cardsList.append(createCard(item, removeCard));
+});
